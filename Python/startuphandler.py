bbf5b31c1343ca9230f7ef1551c0390e5d5afb92
christos-kats <139361833+christos-kats@users.noreply.github.com>
2023-07-27 15:49:05 +0300

    Removed print statements from startuphandler

--- Python/startuphandler.py
+++ Python/startuphandler.py
@@ -6,28 +6,27 @@ from win32com.shell import shell, shellcon
 class StartupHandler:
     def __init__(self):
         startupFolder = os.path.join(os.getenv("APPDATA"), "Microsoft", "Windows", "Start Menu", "Programs", "Startup")
+        self.currentFolder = os.getcwd()
         shortcutName = "Volume Mixer.lnk"
-        self.sourcePath = "main.py"
+        startName = "main.py"
+        self.sourcePath = os.path.join(self.currentFolder,startName)
         self.targetPath = os.path.join(startupFolder, shortcutName)
 
     def addToStartup(self):
-        self.createShortcut(self.sourcePath, self.targetPath)
-        print("addToStartup function called")
+        self.createShortcut(self.sourcePath, self.targetPath, self.currentFolder)
 
     def removeFromStartup(self):
         os.remove(self.targetPath)
-        print("removeFromStartup function called")
 
     def isOnStartup(self):
-        print("isOnStartup function called and returned")
         return os.path.exists(self.targetPath)
     
-    def createShortcut(self, sourcePath, targetPath):
+    def createShortcut(self, sourcePath, targetPath, startIn):
         shortcut = pythoncom.CoCreateInstance(
             shell.CLSID_ShellLink, None,
             pythoncom.CLSCTX_INPROC_SERVER, shell.IID_IShellLink
         )
         shortcut.SetPath(sourcePath)
-
+        shortcut.SetWorkingDirectory(startIn)
         persist_file = shortcut.QueryInterface(pythoncom.IID_IPersistFile)
         persist_file.Save(targetPath, 0)
